c060b34916859c77b48caccd8ac8c258b81ddad2
fortune <omorodion.fortune@gmail.com>
2023-06-01 09:59:17 +0100

    styling header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,47 +1,66 @@
-import logo from "../assets/Laura-Bon-logo.svg";
-import { ShoppingCartIcon } from "@heroicons/react/24/solid";
+import logo from "../assets/logo2.svg";
+import {
+  ShoppingCartIcon,
+  MagnifyingGlassIcon,
+} from "@heroicons/react/24/solid";
+
 import HeaderBottom from "./HeaderBottom";
 export default function Header() {
   return (
-    <nav className="py-1">
-      <div className="text-white p-2 items-center justify-between space-x-3 flex w-full h-[75px]  bg-[rgb(7,47,84)]">
+    <nav className="">
+      <div className="flex text-white relative items-center justify-between space-x-3 w-full h-[60px]  bg-[rgb(7,47,84)]">
         <img
-          className="w-20 h-12 bg-slate-50 ml-5 outline-none"
+          className=" fill w-[167px]  absolute top-0 lg:left-[50px]  ml-2"
           src={logo}
           alt="laurabon logo"
         />
-        <div className="flex items-center gap-5">
-          <ul className="flex gap-11">
-            <li>HOME</li>
-            <li>ABOUT US</li>
-            <li>ARTICLE & VIDEOS</li>
-            <li>CONTACT</li>
+        <div className="flex absolute left-[600px] items-center">
+          <ul className="flex gap-7 items-center">
+            <li>
+              <a href="">PRAYER</a>
+            </li>
+            <div className="h-4 border-white border-l-2 "></div>
+            <li>
+              <a href="">DONATE</a>
+            </li>
+            <div className="h-4 border-white border-l-2 "></div>
+
+            <li className="flex gap-2">
+              <a href="" className="items-center flex gap-2">
+                <ShoppingCartIcon className="h-6" /> View Cart
+              </a>
+            </li>
+            <div className="h-4 border-white border-l-2 "></div>
+            <li>
+              <a href="">Sign In</a>
+            </li>
+            <div className="h-4 border-white border-l-2"></div>
+            <li className="flex ml-2 items-center bg-gray-100 rounded-full p-2">
+              <MagnifyingGlassIcon className="h-6 text-gray-600" />
+              <input
+                type="text"
+                placeholder="Search"
+                className="sm:hidden md:inline-flex ml-3 flex-shrink items-center bg-transparent outline-none placeholder-gray-500"
+              />
+            </li>
           </ul>
-          <button
-            className="flex ml-[250px] mr-5 bg-white p-1 rounded text-blue-900"
-            type="submit"
-          >
-            DONATE
-          </button>
         </div>
       </div>
-      <div className="flex bg-white   h-[75px] mx-auto top-[75px] items-center">
-        <div className="flex mx-auto w-screen justify-center p-[10px] gap-[10px] space-x-2 py-5 items-center">
-          <p className="text-[20px] text-[rgb(3,41,61)]">WATCH + LISTEN</p>
+      <div className="flex sm:hidden shadow-md lg:inline-flex bg-white h-[50px] mx-auto top-[75px] items-center">
+        <div className="flex mx-auto w-screen justify-center p-[10px] space-x-4 py-5 items-center">
+          <p className="text-[20px] font-medium text-[rgb(3,41,61)]">WATCH + LISTEN</p>
           <div className="h-5 border-[rgba(3,41,61,1)] border-l-2 "></div>
-          <p className="text-[20px] text-[rgb(3,41,61)]">LAURABON FOUNDATION</p>
+          <p className="text-[20px] font-medium text-[rgb(3,41,61)]">LAURABON FOUNDATION</p>
           <div className="h-5 border-[rgba(3,41,61,1)] border-l-2 "></div>
-          <p className="text-[20px] text-[rgb(3,41,61)]">STUDY</p>
+          <p className="text-[20px] font-medium text-[rgb(3,41,61)]">STUDY</p>
           <div className="h-5 border-[rgba(3,41,61,1)] border-l-2 "></div>
-          <p className="text-[20px] text-[rgb(3,41,61)] flex gap-2 items-center">
+          <p className="text-[20px] font-medium text-[rgb(3,41,61)] flex gap-2 items-center">
             <ShoppingCartIcon className="h-8 w-8 text-gray-500" />
             SHOP
           </p>
         </div>
       </div>
-      <HeaderBottom className="" />
+      {/* <HeaderBottom className="" /> */}
     </nav>
   );
 }
-
-  
--- src/Components/HeaderBottom.jsx
+++ src/Components/HeaderBottom.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function HeaderBottom() {
   return (
     <>
-      <div className="flex relative bg-[rgb(7,47,84)] items-center h-[80px] justify-center ">
-        <p className="text-white  text-[25px]">
+      <div className="flex relative bg-[rgb(7,47,84)] items-center h-[50px] justify-center ">
+        <p className="text-white  text-[15px]">
           LAURABON FOUNDATION
           <div className="border-b-2 border-white"></div>
         </p>
