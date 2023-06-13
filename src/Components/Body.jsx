fa1b682d5cc6912bc7c8134eed5c45a2072e8f00
fortune <omorodion.fortune@gmail.com>
2023-06-13 16:05:50 +0100

    fixes

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -4,20 +4,24 @@ import bodyImage from "../assets/andra-fuller-wkgv7i2vtzm-unsplash-1.svg";
 function Body() {
   return (
     <section className="">
-      <div className="md:flex w-full md:gap-1 md:p-1 ">
-        
-          <img className="md:w-2/3 pb-2" src={bodyImage} alt="" />
-          <div className="absolute px-[500px] gap-2 p-5 bg-[#0c060630] ">
-            <a href="#" className="absolute rounded p-1  left-32 text-white bg-green-300">
-              Watch Now
-            </a>
-            <a className="p-1 absolute rounded bg-white">Today's Offer</a>
-          </div>
+      <div className="">
+        <div className="relative">
+          <img className="w-full h-auto pb-2" src={bodyImage} alt="" />
+          <button
+            href="#"
+            className="absolute md:uppercase md:right-[500px] right-[250px] bottom-8 md:px-12 md:py-3 rounded md:text-lg text-sm py-2 px-4 text-white bg-[#78BDB8]"
+          >
+            Watch Now
+          </button>
+          <button className="absolute md:uppercase md:left-[500px] left-[250px] bottom-8 rounded bg-white md:text-lg text-sm px-4 md:px-12 md:py-3 py-2">
+            Today's Offer
+          </button>
+        </div>
 
-          <div className="md:flex gap-1 w-full flex-col pb-2">
-            <img className="pb-2 md:p-0" src={bodyImage} alt="" />
-            <img className="" src={bodyImage} alt="" />
-          </div>
+        <div className="gap-1 w-full flex-col pb-2">
+          <img className="pb-2 md:p-0" src={bodyImage} alt="" />
+          <img className="" src={bodyImage} alt="" />
+        </div>
       </div>
     </section>
   );
