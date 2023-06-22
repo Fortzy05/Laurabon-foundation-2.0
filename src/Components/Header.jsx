b9024fc56d5be4693189065c70254cd212e3bb71
fortune <omorodion.fortune@gmail.com>
2023-06-22 16:12:08 +0100

    header fix

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,39 +3,35 @@ import {
   ShoppingCartIcon,
   MagnifyingGlassIcon,
 } from "@heroicons/react/24/solid";
-
 import HeaderCenter from "./HeaderCenter";
+
 export default function Header() {
   return (
     <nav className="sticky top-0 z-50 shadow-lg overflow-hidden">
-      <div className="flex p-2 text-white items-center justify-between space-x-2 w-full h-[42px]  bg-[#072f54] mx-auto">
-        <img className="md:pl-[100px]  h-7 " src={logo} alt="laurabon logo" />
+      <div className="flex text-white items-center justify-between space-x-2 w-full p-2 bg-[#072f54]">
+        <img className="md:pl-[100px] h-7" src={logo} alt="laurabon logo" />
         <div className="md:flex space-x-7 uppercase text-white font-medium md:space-x-11 flex-shrink-0">
-          <ul className="flex gap-1 text-white items-center space-x-2 ">
+          <ul className="flex gap-1 text-white items-center space-x-2">
             <li>
-              <a href="" className="text-sm py-4 px-2">
+              <a href="/" className="text-sm py-4 px-2">
                 PRAYER
               </a>
             </li>
-            <div className="h-3 md:inline-flex border-gray-300 border-l-[1px] hidden"></div>
             <li>
-              <a href="" className="text-sm  py-4 px-2">
+              <a href="/" className="text-sm py-4 px-2">
                 DONATE
               </a>
             </li>
-            <div className="h-3 md:inline-flex border-gray-300 border-l-[1px] hidden "></div>
-
             <li className="flex gap-2">
-              <a href="" className="items-center flex gap-2">
-                <ShoppingCartIcon className="h-6 flex" />
+              <a href="/" className="items-center flex gap-2">
+                <ShoppingCartIcon className="h-6" />
                 <span className="hidden md:flex py-4 px-3 text-sm">
                   View Cart
                 </span>
               </a>
             </li>
-            <div className="h-3 md:inline-flex border-gray-300 border-l-[1px] hidden "></div>
             <li className="hidden md:inline-flex">
-              <a href="" className="py-4 px-3 text-sm">
+              <a href="/" className="py-4 px-3 text-sm">
                 Sign In
               </a>
             </li>
--- src/Components/HeaderCenter.jsx
+++ src/Components/HeaderCenter.jsx
@@ -14,8 +14,8 @@ function HeaderCenter() {
   };
 
   return (
-    <div className="flex h-[50px] items-center bg-white">
-      <div className="">
+    <div className="flex p-2 items-center bg-white relative">
+      
         <button
           id="menu-btn"
           ref={btnRef}
@@ -26,10 +26,15 @@ function HeaderCenter() {
           <div className="hamburger-middle"></div>
           <div className="hamburger-bottom"></div>
         </button>
-        <div id="menu" className="md:hidden">
-          <div
-            className={`{isOpen ? 'open' : ''} ref={navRef} absolute flex-col items-center self-end py-8 mt-10 space-y-6 font-bold bg-gray-900 w-auto left-6 right-6 drop-shadow-md`}
-          >
+
+        <div
+          id="menu"
+          className={`${
+            isOpen ? "open" : "hidden"
+          } absolute top-full mt-11 bg-gray-50 w-full left-6 right-6 drop-shadow-md z-10`}
+          ref={navRef}
+        >
+          <div className="flex flex-col items-center py-8 space-y-6 font-bold">
             <a href="#">Prayer</a>
             <a href="#">Donate</a>
             <a href="#">Shop</a>
@@ -37,7 +42,7 @@ function HeaderCenter() {
             <a href="#">Community</a>
           </div>
         </div>
-      </div>
+    
       <div className="flex md:hidden items-center flex-grow flex-shrink ml-8">
         <form className="flex p-[3px] items-center text-[#574e54] bg-[#e8e8ea] text-sm rounded-full focus:outline-none flex-grow">
           <MagnifyingGlassIcon className="ml-2 h-[18px] text-gray-600" />
@@ -53,20 +58,20 @@ function HeaderCenter() {
         </button>
       </div>
       <div className=" md:flex group cursor-pointer hidden mx-auto text-[#03293d] w-screen justify-center p-[10px] space-x-4 items-center flex-shrink-0">
-        <a href="" className="text-md p-2 ease-in-out">
+        <a href="#" className="text-md p-2 ease-in-out">
           WATCH + LISTEN
         </a>
         <div className="h-4 border-gray-400 border-l-[1px] "></div>
-        <a href="" className="text-md p-4 ease-in-out">
+        <a href="#" className="text-md p-4 ease-in-out">
           LAURABON FOUNDATION
         </a>
         <div className="h-4 border-gray-400 border-l-[1px] "></div>
-        <a href="" className="text-md p-4 ease-in-out">
+        <a href="#" className="text-md p-4 ease-in-out">
           STUDY
         </a>
 
         <div className="h-4 border-gray-400 border-l-[1px] "></div>
-        <a href="" className="text-md p-4 ease-in-out">
+        <a href="#" className="text-md p-4 ease-in-out">
           SHOP
         </a>
       </div>
