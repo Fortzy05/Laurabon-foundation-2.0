6b63287469f0c4dcb112afde4a4594606b926a01
fortune <omorodion.fortune@gmail.com>
2023-07-03 12:21:28 +0100

    foundation page

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -8,8 +8,8 @@ import  pintrest  from "../assets/icons8-pinterest.svg";
 
 function Footer() {
   return (
-    <footer className="bg-[rgb(7,47,84)] md:h-[400px]">
-      <div className="container flex flex-col-reverse justify-between px-6 py-10 mx-auto space-y-8 md:flex-row md:space-y-0">
+    <footer className="absoulte bottom-0 w-full bg-[rgb(7,47,84)] md:h-[400px]">
+      <div className="flex flex-col-reverse justify-between px-6 py-10 mx-auto space-y-8 md:flex-row md:space-y-0">
         <div className="flex flex-col-reverse items-center justify-between space-y-12 md:flex-col md:space-y-0 md:items-start">
           <div className="mx-auto my-6 text-center text-white">
             Copyright &copy; 2023, All Rights Reserved
